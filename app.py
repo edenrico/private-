0727cb9f04df1479a97c16ee440ebc3690b983ed
Eduardo Enrico <143040495+edenrico@users.noreply.github.com>
2024-09-11 10:29:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-# Carregar o modelo Whisper
+
 modelo = whisper.load_model('base')
 
 @app.route('/')
@@ -22,23 +22,23 @@ def upload_file():
         return 'No selected file'
     
     if file:
-        # Salvar o arquivo de áudio
+        
         audio_path = os.path.join('uploads', file.filename)
         file.save(audio_path)
         
-        # Transcrever o áudio
+     
         resposta = modelo.transcribe(audio_path)
         texto = resposta['text']
         
-        # Salvar a transcrição em um arquivo de texto
+        
         output_file = 'transcricao.txt'
         with open(output_file, 'w') as f:
             f.write(texto)
         
-        # Remover o arquivo de áudio após a transcrição
+   
         os.remove(audio_path)
         
-        # Enviar o arquivo de transcrição para download
+      
         return send_file(output_file, as_attachment=True)
 
 if __name__ == '__main__':
